5098777d2199fcb80e15aa3a7f0a2fd38753ddeb
Graceyoung <graceyangting@sina.com>
2016-09-05 17:29:07 +0800

    add class &  change the order of animation
    add class &  change the order of animateIn and animatOut

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -2,12 +2,11 @@
 
 var H5ComponentBase = function(name,cfg){
 	var cfg = cfg ||{};
-	console.log(cfg);
 	var id = ('h5_c_'+Math.random()).replace('.','_');
 
 	//给组件添加class
-	var cls = 'h5_component_'+cfg.type+' h5_component_name_'+name;
-	var component = $('<div class="H5_component '+cls+'" id="'+id+'"></div>');
+	var cls = ' h5_component_'+cfg.type;
+	var component = $('<div class="H5_component h5_component_name_'+name+cls+'" id="'+id+'"></div>');
 
 	cfg.text && component.text(cfg.text);
 	cfg.width && component.width(cfg.width/2);
@@ -23,11 +22,13 @@ var H5ComponentBase = function(name,cfg){
 
 	//载入 移出的动画
 	component.on('onLeave',function(){
-		cfg.animateIn && component.animate(cfg.animateIn);
+		$(this).removeClass(cls+'_load').addClass(cls+'_leave');
+		cfg.animateIn && component.animate(cfg.animateOut);
 		return false;
 	});
 	component.on('onLoad',function(){
-		cfg.animateIn && component.animate(cfg.animateOut);
+		$(this).removeClass(cls+'_leave').addClass(cls+'_load');
+		cfg.animateIn && component.animate(cfg.animateIn);
 		return false;
 	});
 

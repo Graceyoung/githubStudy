fcea6e22ee5241a3def08e72851db51aca9f9133
Graceyoung <graceyangting@sina.com>
2016-09-10 10:24:55 +0800

    add animation
    let point move and keep the shadow changing all the time

--- js/H5ComponentPoint.js
+++ js/H5ComponentPoint.js
@@ -15,6 +15,7 @@ var H5ComponentPoint = function(name,cfg){
 		var whper = (item[1]/base*100)+'%';
 
 		point.width(whper).height(whper);//设置散点图的宽高
+		point.css('z-index',cfg.data.length-index);//point的层次显示
 		
 
 		if(item[2]){
@@ -22,9 +23,10 @@ var H5ComponentPoint = function(name,cfg){
 			point.css('background-color',item[2]);
 
 		}
-		if(item[3]!=undefined && item[4]!=undefined){
 
-			point.css('left',item[3]).css('top',item[4]);
+		// 将要做动画的point设置在中心点，从中心点向外运动
+		if(item[3]!=undefined && item[4]!=undefined){
+			point.css('margin',cfg.height*(1-item[1]/base)/4+'px '+cfg.width*(1-item[1]/base)/4+'px');
 		}
 
 		//放文字的框框，一个大框，一个小框
@@ -37,7 +39,37 @@ var H5ComponentPoint = function(name,cfg){
 		point.append(name);
 		component.append(point);
 	});
-	
 
+	function pointAnimate(per){
+		$.each(cfg.data,function(index,item){
+			if(item[3]!=undefined && item[4]!=undefined){
+				var left = item[3]*per +'%';
+				var top = item[4]*per +'%';
+				console.log();
+				$('.point_'+index).css('left',left).css('top',top);
+			}
+		});
+	}
+	
+	
+	component.on('onLoad',function(){
+		var s = 0 ;
+		for(var i=0;i<100;i++){
+			setTimeout(function(){
+				s += 0.01;
+				pointAnimate(s);
+			},i*10+500);
+		}
+	});
+	component.on('onLeave',function(){
+		var s = 1 ;
+		for(var i=0;i<100;i++){
+			setTimeout(function(){
+					s -= 0.01;
+					pointAnimate(s);
+			},i*10+500);
+		}
+	});
+	
 	return component;
 }
